451cf3c99e29d43a6b0bad3f21aab538006d239b
paullessing <paul@paullessing.com>
2018-07-21 21:12:40 +0100

    Introduce @Middleware
    Apply Middleware to methods without a path
    
    Add tests for Middleware

--- src/use-routes.ts
+++ src/use-routes.ts
@@ -1,17 +1,57 @@
 import { IRouterMatcher, Router } from 'express';
 import 'reflect-metadata';
-import { METADATA_KEY_METHODS, MethodDefinition } from './metadata';
+import {
+  isMethodDefinition,
+  isMiddlewareDefinition,
+  METADATA_KEY_METHODS,
+  MetadataDefinition,
+  MethodDefinition
+} from './metadata';
+
+const getMetadataByProperty = (definitions: MetadataDefinition[]) => {
+  const definitionsByProperty: {
+    propertyName: string | symbol;
+    definitions: MetadataDefinition[];
+  }[] = [];
+
+  outer: for (const definition of definitions) {
+    for (const mapEntry of definitionsByProperty) {
+      if (mapEntry.propertyName === definition.property) {
+        mapEntry.definitions.push(definition);
+        continue outer;
+      }
+    }
+    definitionsByProperty.push({
+      propertyName: definition.property,
+      definitions: [definition]
+    });
+  }
+
+  return definitionsByProperty;
+};
 
 export function useRoutes(app: Router, routerInstance: object): Router {
   if (!Reflect.hasMetadata(METADATA_KEY_METHODS, routerInstance)) {
     return app;
   }
 
-  const definitions: MethodDefinition[] = Reflect.getMetadata(METADATA_KEY_METHODS, routerInstance);
-  for (const definition of definitions) {
-    const property = (routerInstance as any)[definition.property].bind(routerInstance);
-    const verb = definition.method.toLowerCase();
-    ((app as any)[verb] as IRouterMatcher<any>)(definition.path, property);
+  const definitions: MetadataDefinition[] = Reflect.getMetadata(METADATA_KEY_METHODS, routerInstance);
+  const metadataByProperty = getMetadataByProperty(definitions);
+
+  for (const metadata of metadataByProperty) {
+    const propertyName = metadata.propertyName;
+    const property = (routerInstance as any)[propertyName].bind(routerInstance);
+
+    const middleware = metadata.definitions
+      .filter(isMiddlewareDefinition)
+      .map((definition) => definition.handler)
+      .reverse(); // Method decorators are evaluated in reverse order
+    const methods = metadata.definitions.filter<MethodDefinition>(isMethodDefinition);
+
+    for (const methodMetadata of methods) {
+      const verb = methodMetadata.method.toLowerCase() as keyof Router;
+      (app[verb] as IRouterMatcher<any>).apply(app, [methodMetadata.path, ...middleware, property]);
+    }
   }
 
   return app;
