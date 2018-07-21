f53afc1ba66018cc808f3083b9020a36120a3177
paullessing <paul@paullessing.com>
2018-07-21 19:49:23 +0100

    Add multi-binding test

--- test/use-routes.spec.ts
+++ test/use-routes.spec.ts
@@ -1,4 +1,4 @@
-import { Get, useRoutes } from '../src';
+import { Delete, Get, Post, useRoutes } from '../src';
 import express from 'express';
 import request from 'supertest';
 import { createRouter } from './test-helper';
@@ -60,5 +60,40 @@ describe('useRoutes()', () => {
 
     expect(response.status).toBe(200);
     expect(response.body).toEqual(body);
+  });
+
+  it('should map one method to multiple routes if they are all bound', async () => {
+    class MultiRoutes {
+      @Get('/hello')
+      @Get('/bye')
+      @Post('/hello')
+      @Delete('/greeting')
+      public manage: jest.Mock;
+
+      constructor() {
+        this.manage = jest.fn();
+        this.manage.mockImplementation((_: express.Request, res: express.Response) => {
+          res.end();
+        });
+      }
+    }
+
+    const router = new MultiRoutes();
+    const app = createRouter(router);
+
+    await request(app).get('/hello');
+    await request(app).get('/hello');
+    await request(app).post('/hello');
+    await request(app).delete('/greeting');
+
+    expect(router.manage).toHaveBeenCalledTimes(4);
+    expect(router.manage.mock.calls[0][0].method).toBe('GET');
+    expect(router.manage.mock.calls[0][0].path).toBe('/hello');
+    expect(router.manage.mock.calls[1][0].method).toBe('GET');
+    expect(router.manage.mock.calls[1][0].path).toBe('/hello');
+    expect(router.manage.mock.calls[2][0].method).toBe('POST');
+    expect(router.manage.mock.calls[2][0].path).toBe('/hello');
+    expect(router.manage.mock.calls[3][0].method).toBe('DELETE');
+    expect(router.manage.mock.calls[3][0].path).toBe('/greeting');
   });
 });
